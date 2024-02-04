e38c903aea8f887f610f69acc55626eb7810b7d4
KisVan9705 <kvandari9705@gmail.com>
2024-02-04 14:11:23 -0600

    save function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let countEl = document.getElementById("increment");
+let saveEl = document.querySelector("#save");
 let count = 0;
 function Increment(){
     count++
     countEl.innerHTML= count;
+}
+
+function save(){
+    saveEl.innerHTML += count + " - ";
+    count = 0;
+    countEl.textContent= count;
 }
